4d07daca9a052f15ee3fa7d4579274cf069f47be
Satriobalappisang <69732095+summer196@users.noreply.github.com>
2026-02-27 10:09:13 +0700

    push pertemuan ke 4

---
+++ Pertemuan4-DisctionaryKeyValue/clear().py
@@ -0,0 +1,7 @@
+'''Clear'''
+#Menghapus semua elemen dari dictionary
+
+kontak = {'Kiara': '82114352867', 'Rere': '081290967867', 'Michel': '081295908579'}
+print("Kontak sebelum dihapus", kontak)
+kontak.clear()
+print('kontak setelah di hapus:', kontak)
